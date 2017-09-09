053fa09716eecc2e720f1b654eb03287d407f114
goha <gohavorsin@gmail.com>
2017-09-09 22:28:42 +1000

    end

---
+++ Database.php
@@ -0,0 +1,135 @@
+<?php
+
+session_start();
+$_SESSION['user_id'] = 1;
+
+class Database{
+    public $isConn;
+    protected $datab;
+
+    public function __construct($username = "root", $password = "", $host = "localhost", $dbname = "todo", $options = []){
+        $this->isConn = TRUE;
+        try {
+            $this->datab = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
+            $this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+
+    }
+
+
+    public function Disconnect(){
+        $this->datab = NULL;
+        $this->isConn = FALSE;
+    }
+
+
+    public function getItems()
+    {
+        try {
+            $itemsQuery = $this->datab->prepare("
+            SELECT id, name, done
+            FROM items
+            WHERE user = :user
+            ");
+            $itemsQuery->execute([
+                'user' => $_SESSION['user_id']
+            ]);
+            $item = $itemsQuery->rowCount() ? $itemsQuery : [];
+
+            return $item;
+
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function addItem($nameItem){
+        if(isset($_POST['name'])) {
+            $name = trim($_POST['name']);
+
+            if(!empty($name)) {
+                try {
+                    $addedQuery = $this->datab->prepare("
+                        INSERT INTO items (name, user, done, created)
+                        VALUES (:name, :user, 0, NOW())
+                    ");
+
+                    $addedQuery->execute([
+                        'name' => $name,
+                        'user' => $_SESSION['user_id']
+                    ]);
+
+                } catch (PDOException $e) {
+                    throw new Exception($e->getMessage());
+                }
+            }
+        }
+
+        header('Location: index.php');
+    }
+
+
+
+    public function markItem($as,$item){
+
+        switch ($as) {
+            case 'done':
+                $doneQuery = $this->datab->prepare("
+            UPDATE items
+            SET done = 1
+            WHERE id = :item
+            AND user = :user
+        ");
+
+                $doneQuery->execute([
+                    'item' => $item,
+                    'user' => $_SESSION['user_id']
+                ]);
+                break;
+        }
+
+
+        header('Location: index.php');
+    }
+
+    public function delItem($as,$item){
+
+        switch ($as) {
+            case 'delete':
+                $doneQuery = $this->datab->prepare("
+				DELETE FROM items
+				WHERE id =:item
+				AND user = :user
+			");
+
+                $doneQuery->execute([
+                    'item' => $item,
+                    'user' => $_SESSION['user_id']
+                ]);
+                break;
+        }
+
+
+        header('Location: index.php');
+    }
+}
+
+$db = new Database();
+
+if(isset($_GET['as'], $_GET['item'])) {
+    if($_GET['as'] == 'done'){
+        $db->markItem($_GET['as'],$_GET['item']);
+    }
+    if($_GET['as'] == 'delete'){
+        $db->delItem($_GET['as'],$_GET['item']);
+    }
+}
+
+if(!isset($_SESSION['user_id'])){
+    die('Вы не вошли в аккаунт.');
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'Database.php';
+
+$db = new Database();
+
+$items = $db->getItems();
+
+if($_POST['add']){
+    $db->addItem($_POST['name']);
+}
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ToDo List</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+	<meta name="vierport" content="width=device-width,user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
+
+</head>
+<body>
+	<div class="list">
+		<h1 class="header">To do.</h1>
+
+		<?php if(!empty($items)): ?>
+
+		<ul class="items">
+			<?php foreach ($items as $item): ?>
+				<li>
+					<span class="item<?php echo $item['done'] ? ' done' : ''; ?>">
+						<?php echo $item['name'];?>
+					</span>
+					<?php if(!$item['done']): ?>
+						<a href="Database.php?as=done&item=<?php echo $item['id']; ?>" class="done-button">Выполнено</a>
+					<?php endif; ?>
+					<a href="Database.php?as=delete&item=<?php echo $item['id']; ?>" class="delete-button">Удалить</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+		<?php else: ?>
+			<p>Вы еще не добавили какое-либо задание!</p>
+		<?php endif; ?>
+
+		<form class="item-add" method="POST">
+			<input type="text" name="name" placeholder="Что тебе нужно сделать?" class="input">
+			<input type="submit" name="add" value="Добавить" class="submit">
+		</form>
+	</div>
+</body>
+</html>
